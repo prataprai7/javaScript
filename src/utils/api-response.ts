a2e4436c6e170586cb8a308b6c51d4d4bfb396d8
pratap <pratapbantawa3@gmail.com>
2026-04-24 09:34:43 +0545

    utils

---
+++ src/utils/api-response.ts
@@ -0,0 +1,14 @@
+//Consistent API
+const res_example={
+    "status":200,
+    "data":{
+        //..data
+    },
+    "message":"Success",
+    "meat":{
+        //pagination,etc
+        "page":1,
+        "limit":10,
+        "total":100
+    }
+}
