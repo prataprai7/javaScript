cd64b582f3ef4c50bc3e410550de9ce8f41425cd
pratap <pratapbantawa3@gmail.com>
2026-04-29 09:35:25 +0545

    repositories

---
+++ src/repositories/person.repository.ts
@@ -0,0 +1,23 @@
+import { Person } from "../types/person.type";
+import { data } from "../models/person.model";
+
+// database/source contract
+interface IPersonRepository {
+    getAll(): Person[];
+    getOne(id: string): Person | undefined;
+    create(person: Person): Person | undefined;
+}
+
+export class PersonArrayRepository implements IPersonRepository{
+    getAll(): Person[] {
+        return data;
+    }
+    getOne(id: string): Person | undefined {
+        const found = data.find(p => p.id === parseInt(id));
+        return found;
+    }
+    create(person: Person): Person | undefined {
+        data.push(person);
+        return person;
+    }
+}
